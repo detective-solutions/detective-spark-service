5735b4615d8358ee9ea33ee20a42bf06621218ab
Maxim Perl <maxim.perl@outlook.de>
2020-10-25 09:07:48 +0100

    fix(DET-467): port mapping of detective-services in dockerfile

--- test.py
+++ test.py
@@ -44,4 +44,4 @@ api.add_resource(DataBricksRequest, '/ddl')
 
 # run app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
